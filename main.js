a701ed82acfa8cf623f5d051e593026848ba75f7
Dark-Knigth-maker <78041177+Dark-Knigth-maker@users.noreply.github.com>
2021-05-06 16:18:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,3 +2,16 @@
 window.addEventListener("load", function(){
     document.getElementById("loader").classList.toggle("loader2")
 })
+
+// Nav
+const btnToggle = document.querySelector(".nav__icon-btn");
+
+btnToggle.addEventListener("click", function () {
+    document.getElementById("nav__ul").classList.toggle("active");
+});
+
+const btnResponsive = document.querySelector(".nav__responsive-button");
+
+btnResponsive.addEventListener("click", function () {
+    document.getElementById("nav__responsive-ul").classList.toggle("active");
+});
